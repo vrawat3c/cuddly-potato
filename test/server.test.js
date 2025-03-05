adb1b072843069150009f6be5fdc14c2bfa6145e
vrawat3c <vrawat@3clogic.com>
2025-03-05 17:59:41 +0530

    fix

---
+++ test/server.test.js
@@ -0,0 +1,8 @@
+import {assert} from 'chai';
+import { describe, it } from 'mocha';
+
+describe('Server Tests', () => {
+    it('hello world!', () => {
+        assert.strictEqual(1 + 1, 2);
+    });
+});
